a0952e77f830e3de7c4b55fb403a175dbc9df682
KJY <KJY@DESKTOP-FMFUT7B>
2022-12-28 17:41:55 +0900

    백업

--- GreenAcademi/src/greenAcademiGolf/MainUI.java
+++ GreenAcademi/src/greenAcademiGolf/MainUI.java
@@ -14,20 +14,24 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.ArrayList;
 
 public class MainUI extends WindowAdapter implements ActionListener {
 	private Frame firstFrame, joinFrame, searchFrame, mainFrame;
-	private Button ff_bLogin, ff_bJoin, ff_bFind, bt2_bCancel, bt2_bLogin, jf_bJoin, jf_bLogin, sf_bFindID, sf_bFindPwd,
+	private Button ff_bLogin, ff_bJoin, ff_bFind, bt2_bCancel, bt2_bLogin, jf_bJoin, jf_bLogin, jf_bIDchk, jf_bPWDchk,
+			d3_idchk_bok, d3_idchk_bno, d3_pwdchk_bok, d3_pwdchk_bno, d3_joinComplet_bok, sf_bFindID, sf_bFindPwd,
 			sf_bBack, sf_pID_bConfirm, sf_pID_bCancel, sf_pPwd_bConfirm, sf_pPwd_bCancel, d1_bOK, d2_bOK1, d2_bOK2,
 			mf_pM_bHome, mf_pM_bRecord, mf_pM_bStats, mf_pM_bSwing, mf_pM_bMap, mf_pH_bReser;
 	private Panel p_1stMain, fst_pBottom1, fst_pBottom2, sf_pFindID, sf_pFindPwd, mf_pMenu, mf_pHome, mf_pRecord,
 			mf_pStat, mf_pVideo, mf_pMap;
-	private Label bt2_lID, bt2_lPwd, jf_lJoin, jf_lID, jf_lName, jf_lEmail, jf_lPwd, jf_lRepwd, sf_pID_lName,
-			sf_pID_lEmail, sf_pPwd_lID, sf_pPwd_lEmail, d1_lMsg1, d2_lSuccess, d2_lFail, mf_pH_lTitle;
+	private Label bt2_lID, bt2_lPwd, jf_lJoin, jf_lID, jf_lName, jf_lEmail, jf_lPwd, jf_lRepwd, d3_idchk_lok,
+			d3_idchk_lno, d3_pwdchk_lok, d3_pwdchk_lno, d3_joinComplet_ljc, sf_pID_lName, sf_pID_lEmail, sf_pPwd_lID,
+			sf_pPwd_lEmail, d1_lMsg1, d2_lSuccess, d2_lFail, mf_pH_lTitle;
 	private TextField bt2_tID, bt2_tPwd, jf_tID, jf_tName, jf_tEmail, jf_tPwd, jf_tRepwd, sf_pID_tName, sf_pID_tEmail,
 			sf_pPwd_tID, sf_pPwd_tEmail;
 	private TextArea mf_pH_taSum, mf_pH_taVideo;
-	private Dialog d1_LoginFail, d2_joinSuccess, d2_joinFail;
+	private Dialog d1_LoginFail, d2_joinSuccess, d2_joinFail, d3_jf_idchk_ok, d3_jf_idchk_no, d3_jf_pwdchk_ok,
+			d3_jf_pwdchk_no, d3_jf_joinComplet;
 
 	MainUI() {
 
@@ -109,28 +113,60 @@ public class MainUI extends WindowAdapter implements ActionListener {
 
 		// 프레임 2(회원가입 창)
 		joinFrame = new Frame("join Frame");
-		joinFrame.setLayout(new GridLayout(13, 0));
+		joinFrame.setLayout(null);
 		joinFrame.setSize(700, 850);
 		joinFrame.setVisible(false);
 
 		// 프레임 2 -> 회원가입 정보 입력 라벨, 텍스트박스
 		jf_lJoin = new Label("jf_lJoin", Label.CENTER);
+		jf_lJoin.setSize(700, 100);
+		jf_lJoin.setLocation(0, 0);
 		jf_lID = new Label("jf_lID", Label.LEFT);
+		jf_lID.setSize(100, 100);
+		jf_lID.setLocation(0, 100);
 		jf_tID = new TextField(10);
+		jf_tID.setSize(500, 100);
+		jf_tID.setLocation(100, 100);
 		jf_lName = new Label("jf_lName", Label.LEFT);
+		jf_lName.setSize(100, 100);
+		jf_lName.setLocation(0, 200);
 		jf_tName = new TextField(10);
+		jf_tName.setSize(500, 100);
+		jf_tName.setLocation(100, 200);
 		jf_lEmail = new Label("jf_lEmail", Label.LEFT);
+		jf_lEmail.setSize(100, 100);
+		jf_lEmail.setLocation(0, 300);
 		jf_tEmail = new TextField(10);
+		jf_tEmail.setSize(500, 100);
+		jf_tEmail.setLocation(100, 300);
 		jf_lPwd = new Label("jf_lPwd", Label.LEFT);
+		jf_lPwd.setSize(100, 100);
+		jf_lPwd.setLocation(0, 400);
 		jf_tPwd = new TextField(10);
+		jf_tPwd.setSize(500, 100);
+		jf_tPwd.setLocation(100, 400);
 		jf_lRepwd = new Label("jf_lRepwd", Label.LEFT);
+		jf_lRepwd.setSize(100, 100);
+		jf_lRepwd.setLocation(0, 500);
 		jf_tRepwd = new TextField(10);
+		jf_tRepwd.setSize(500, 100);
+		jf_tRepwd.setLocation(100, 500);
 
 		// 프레임2 -> 버튼
+		jf_bIDchk = new Button("jf_bIDchk");
+		jf_bIDchk.setSize(100, 100);
+		jf_bIDchk.setLocation(600, 100);
+		jf_bPWDchk = new Button("jf_bPWDchk");
+		jf_bPWDchk.setSize(100, 100);
+		jf_bPWDchk.setLocation(600, 500);
 		jf_bJoin = new Button("jf_bJoin");
+		jf_bJoin.setSize(100, 100);
+		jf_bJoin.setLocation(100, 700);
 		jf_bLogin = new Button("jf_bLogin");
+		jf_bLogin.setSize(100, 100);
+		jf_bLogin.setLocation(300, 700);
 
-		// 프레임2 -> 가입 성공, 실패 다이얼로그
+		// 프레임2 -> 다이얼로그
 		d2_joinSuccess = new Dialog(joinFrame, false);
 		d2_joinSuccess.setLayout(new FlowLayout());
 		d2_joinSuccess.setSize(300, 200);
@@ -144,6 +180,41 @@ public class MainUI extends WindowAdapter implements ActionListener {
 		d2_joinFail.setLocation(150, 300);
 		d2_lFail = new Label("Fail!\nReTry!!");
 		d2_bOK2 = new Button("OK2");
+
+		d3_jf_idchk_ok = new Dialog(joinFrame, false);
+		d3_jf_idchk_ok.setLayout(new FlowLayout());
+		d3_jf_idchk_ok.setSize(300, 200);
+		d3_jf_idchk_ok.setLocation(150, 300);
+		d3_idchk_lok = new Label("d3_idchk_lok");
+		d3_idchk_bok = new Button("d3_idchk_bok");
+
+		d3_jf_idchk_no = new Dialog(joinFrame, false);
+		d3_jf_idchk_no.setLayout(new FlowLayout());
+		d3_jf_idchk_no.setSize(300, 200);
+		d3_jf_idchk_no.setLocation(150, 300);
+		d3_idchk_lno = new Label("d3_idchk_lno");
+		d3_idchk_bno = new Button("d3_idchk_bno");
+
+		d3_jf_pwdchk_ok = new Dialog(joinFrame, false);
+		d3_jf_pwdchk_ok.setLayout(new FlowLayout());
+		d3_jf_pwdchk_ok.setSize(300, 200);
+		d3_jf_pwdchk_ok.setLocation(150, 300);
+		d3_pwdchk_lok = new Label("d3_pwdchk_lok");
+		d3_pwdchk_bok = new Button("d3_pwdchk_bok");
+
+		d3_jf_pwdchk_no = new Dialog(joinFrame, false);
+		d3_jf_pwdchk_no.setLayout(new FlowLayout());
+		d3_jf_pwdchk_no.setSize(300, 200);
+		d3_jf_pwdchk_no.setLocation(150, 300);
+		d3_pwdchk_lno = new Label("d3_pwdchk_lno");
+		d3_pwdchk_bno = new Button("d3_pwdchk_bno");
+
+		d3_jf_joinComplet = new Dialog(joinFrame, false);
+		d3_jf_joinComplet.setLayout(new FlowLayout());
+		d3_jf_joinComplet.setSize(300, 200);
+		d3_jf_joinComplet.setLocation(150, 300);
+		d3_joinComplet_ljc = new Label("d3_joinComplet_ljc");
+		d3_joinComplet_bok = new Button("d3_joinComplet_bok");
 
 		// 프레임 3(아이디/비밀번호 찾기 창)
 		searchFrame = new Frame("search Frame");
@@ -319,6 +390,8 @@ public class MainUI extends WindowAdapter implements ActionListener {
 
 		bt2_bLogin.addActionListener(this);
 		bt2_bCancel.addActionListener(this);
+		jf_bIDchk.addActionListener(this);
+		jf_bPWDchk.addActionListener(this);
 		jf_bJoin.addActionListener(this);
 		jf_bLogin.addActionListener(this);
 
@@ -328,6 +401,12 @@ public class MainUI extends WindowAdapter implements ActionListener {
 		d2_bOK2.addActionListener(this);
 
 		joinFrame.addWindowListener(this);
+
+		d3_idchk_bok.addActionListener(this);
+		d3_idchk_bno.addActionListener(this);
+		d3_pwdchk_bok.addActionListener(this);
+		d3_pwdchk_bno.addActionListener(this);
+		d3_joinComplet_bok.addActionListener(this);
 
 		fst_pBottom2.add(bt2_lID);
 		fst_pBottom2.add(bt2_tID);
@@ -348,16 +427,29 @@ public class MainUI extends WindowAdapter implements ActionListener {
 		joinFrame.add(jf_lJoin);
 		joinFrame.add(jf_lID);
 		joinFrame.add(jf_tID);
+		joinFrame.add(jf_bIDchk);
 		joinFrame.add(jf_lName);
 		joinFrame.add(jf_tName);
 		joinFrame.add(jf_lEmail);
 		joinFrame.add(jf_tEmail);
 		joinFrame.add(jf_lPwd);
 		joinFrame.add(jf_tPwd);
+		joinFrame.add(jf_bPWDchk);
 		joinFrame.add(jf_lRepwd);
 		joinFrame.add(jf_tRepwd);
 		joinFrame.add(jf_bJoin);
 		joinFrame.add(jf_bLogin);
+
+		d3_jf_idchk_ok.add(d3_idchk_lok);
+		d3_jf_idchk_ok.add(d3_idchk_bok);
+		d3_jf_idchk_no.add(d3_idchk_lno);
+		d3_jf_idchk_no.add(d3_idchk_bno);
+		d3_jf_pwdchk_ok.add(d3_pwdchk_lok);
+		d3_jf_pwdchk_ok.add(d3_pwdchk_bok);
+		d3_jf_pwdchk_no.add(d3_pwdchk_lno);
+		d3_jf_pwdchk_no.add(d3_pwdchk_bno);
+		d3_jf_joinComplet.add(d3_joinComplet_ljc);
+		d3_jf_joinComplet.add(d3_joinComplet_bok);
 
 		searchFrame.addWindowListener(this);
 		searchFrame.add(sf_bFindID);
@@ -416,23 +508,44 @@ public class MainUI extends WindowAdapter implements ActionListener {
 	}
 
 	public void actionPerformed(ActionEvent e) {
+		MemberDAO dao = new MemberDAO();
+
 		System.out.println("click!");
+
 		if (e.getActionCommand().equals(ff_bLogin.getLabel())) {
 			firstFrame.remove(fst_pBottom1);
 			firstFrame.add(fst_pBottom2);
 		}
+
 		if (e.getActionCommand().equals(ff_bFind.getLabel())) {
 			searchFrame.setVisible(true);
 		}
-		if (e.getActionCommand().equals(bt2_bLogin.getLabel())) {
-			if (bt2_tID.getText().equals("asdasd") && bt2_tPwd.getText().equals("asdasd")) {
-				System.out.println("Login success -> main page");
+
+		if (e.getActionCommand().equals(bt2_bLogin.getLabel())) { // **로그인 버튼**
+			ArrayList<MemberVo> list = dao.tryLogin(bt2_tID.getText());
+			String mem_pwd = "";
+			for (int i = 0; i < list.size(); i++) {
+				MemberVo data = (MemberVo) list.get(i);
+				mem_pwd = data.getMem_pwd();
+			}
+			if (bt2_tPwd.getText().equals(mem_pwd)) {
 				firstFrame.setVisible(false);
 				mainFrame.setVisible(true);
 			} else {
 				d1_LoginFail.setVisible(true);
 			}
 		}
+
+		if (e.getActionCommand().equals(jf_bIDchk.getLabel())) { // **회원가입 IDchk 버튼**
+			if ((dao.idChk(jf_tID.getText())) == false) {
+				System.out.println("retry");
+				d3_jf_idchk_no.setVisible(true);
+				jf_tID.setText("");
+			} else {
+				d3_jf_idchk_ok.setVisible(true);
+			}
+		}
+
 		if (e.getActionCommand().equals(d1_bOK.getLabel())) {
 			d1_LoginFail.setVisible(false);
 			bt2_tID.setText("");
@@ -446,13 +559,18 @@ public class MainUI extends WindowAdapter implements ActionListener {
 		if (e.getActionCommand().equals(ff_bJoin.getLabel())) {
 			joinFrame.setVisible(true);
 		}
-		if (e.getActionCommand().equals(jf_bJoin.getLabel())) {
-			if (jf_tID.getText().equals("asdasd")) {
-				System.out.println("Join->DB connect");
-				d2_joinSuccess.setVisible(true);
-			} else {
-				d2_joinFail.setVisible(true);
-			}
+		if (e.getActionCommand().equals(jf_bJoin.getLabel())) { // **회원가입 버튼**
+			String new_id = jf_tID.getText();
+			String new_name = jf_tName.getText();
+			String new_email = jf_tEmail.getText();
+			String new_pwd = jf_tPwd.getText();
+			dao.joinMembership(new_id, new_name, new_email, new_pwd);
+			d3_jf_joinComplet.setVisible(true);
+			jf_tID.setText("");
+			jf_tName.setText("");
+			jf_tEmail.setText("");
+			jf_tPwd.setText("");
+			jf_tRepwd.setText("");
 		}
 		if (e.getActionCommand().equals(jf_bLogin.getLabel())) {
 			System.out.println("Login");
@@ -528,6 +646,34 @@ public class MainUI extends WindowAdapter implements ActionListener {
 			mf_pStat.setVisible(false);
 			mf_pVideo.setVisible(false);
 			mf_pMap.setVisible(true);
+		}
+		if (e.getActionCommand().equals(d3_idchk_bok.getLabel())) {
+			d3_jf_idchk_ok.dispose();
+		}
+		if (e.getActionCommand().equals(d3_idchk_bno.getLabel())) {
+			d3_jf_idchk_no.dispose();
+		}
+		if (e.getActionCommand().equals(jf_bPWDchk.getLabel())) {
+			if (jf_tPwd.getText().equals(jf_tRepwd.getText())) {
+				System.out.println("good");
+				d3_jf_pwdchk_ok.setVisible(true);
+			} else {
+				System.out.println("retry");
+				d3_jf_pwdchk_no.setVisible(true);
+				jf_tPwd.setText("");
+				jf_tRepwd.setText("");
+			}
+		}
+
+		if (e.getActionCommand().equals(d3_pwdchk_bok.getLabel())) {
+			d3_jf_pwdchk_ok.dispose();
+		}
+
+		if (e.getActionCommand().equals(d3_pwdchk_bno.getLabel())) {
+			d3_jf_pwdchk_no.dispose();
+		}
+		if (e.getActionCommand().equals(d3_joinComplet_bok.getLabel())) {
+			d3_jf_joinComplet.dispose();
 		}
 	}
 
